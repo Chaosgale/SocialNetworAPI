94fd195654b728a3015370e5b140b7034dcca167
Yan Ashe <yanashe@Yans-Mac-Studio.local>
2024-05-21 20:19:56 -0500

    added Reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const {Schema, Types} = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLenght: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString(),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+mudule.exports = reactionSchema;
