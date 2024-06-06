c8a7fd9962cfee55df5216a8042e2e089ae1f8d2
Chaosgale <yantus@gmail.com>
2024-06-06 03:19:11 -0500

    added thoughts seeds

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { User, Thought, Reaction } = require('../models');
+const { User, Thought } = require('../models');
 
 const users = [
     {
@@ -16,6 +16,24 @@ const users = [
     },
 ];
 
+const thoughts = [
+    {
+        thoughtText: 'This is a thought from Alice',
+        username: 'Alice',
+        reactions: [],
+    },
+    {
+        thoughtText: 'This is a thought from Bob',
+        username: 'Bob',
+        reactions: [],
+    },
+    {
+        thoughtText: 'This is a thought from Charlie',
+        username: 'Charlie',
+        reactions: [],
+    },
+];
+
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
@@ -26,7 +44,10 @@ connection.once('open', async () => {
     await User.deleteMany({});
   
     // Insert new users
-    await User.collection.insertMany(users);
+    const insertedUsers = await User.collection.insertMany(users);
+
+    // Insert new thoughts
+    const insertedThoughts = await Thought.collection.insertMany(thoughts);
 
     console.info('Seeding complete! 🌱');
     process.exit(0);
